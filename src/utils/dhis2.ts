6a5cb8595af3322b7f355cfb70dce03a3a2ca508
Belendia Serda <belendia@gmail.com>
2023-02-10 09:01:30 +0300

    Change period for low transmission season

--- src/utils/dhis2.ts
+++ src/utils/dhis2.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getData = (orgUnit: String) => {
-  const url = `${process.env.DHIS2_ANALYTICS_API_URL}?dimension=dx:SyQlNwchFeS&dimension=pe:LAST_12_MONTHS;THIS_MONTH;&dimension=ou:${orgUnit}&dimension=co:u1uT2mTdt6Q`;
+  const url = `${process.env.DHIS2_ANALYTICS_API_URL}?dimension=dx:SyQlNwchFeS&dimension=pe:202302;202303;202304;202305;202306;202307;202308;202309;202310;202311;202312;&dimension=ou:${orgUnit}&dimension=co:u1uT2mTdt6Q`;
   const token = `${process.env.DHIS2_USERNAME}:${process.env.DHIS2_PASSWORD}`;
   const encodedToken = Buffer.from(token).toString("base64");
   const headers = { Authorization: "Basic " + encodedToken };
